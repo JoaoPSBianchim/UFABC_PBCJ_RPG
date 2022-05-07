7ccb02f7df8ea7bda156c0e4edf506a94a12c8cc
Jonathan Takeshi Ywahashi <jonathan.ywahashi@gmail.com>
2022-05-07 17:15:10 -0300

    fix: fixes inventory initial slot update
    - Adjusts better collision detector for player.

--- RPG/Assets/Scripts/Monobehaviours/Inventario.cs
+++ RPG/Assets/Scripts/Monobehaviours/Inventario.cs
@@ -15,11 +15,14 @@ public class Inventario : MonoBehaviour
         CriaSlots();
     }
 
+    /// <summary>
+    /// Cria os slots do inventário.
+    /// </summary>
     public void CriaSlots()
     {
         if (slotPrefab != null)
         {
-            for(int i = 0; i < numSlots; i++)
+            for (int i = 0; i < numSlots; i++)
             {
                 GameObject novoSlot = Instantiate(slotPrefab);
                 novoSlot.name = "ItemSlot_" + i;
@@ -30,28 +33,43 @@ public class Inventario : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Adiciona um item ao inventário.
+    /// </summary>
+    /// <param name="itemToAdd">item a ser adicionado</param>
+    /// <returns></returns>
     public bool AddItem(Item itemToAdd)
     {
-        for(int i = 0; i< items.Length; i++)
+        for (int i = 0; i < items.Length; i++)
         {
-            if(items[i] != null && items[i].tipoItem == itemToAdd.tipoItem && itemToAdd.empilhavel == true)
-            {  
+            if (items[i] != null && items[i].tipoItem == itemToAdd.tipoItem && itemToAdd.empilhavel == true)
+            {
                 items[i].quantidade = items[i].quantidade + 1;
-                Slot slotScript = slots[i].gameObject.GetComponent<Slot>();
-                Text quantidadeTexto = slotScript.qtdTexto;
-                quantidadeTexto.enabled = true;
-                quantidadeTexto.text = items[i].quantidade.ToString();
+                UpdateSlotText(i);
                 return true;
             }
-            if(items[i] == null)
+            if (items[i] == null)
             {
                 items[i] = Instantiate(itemToAdd);
                 items[i].quantidade = 1;
                 itemImagens[i].sprite = itemToAdd.sprite;
                 itemImagens[i].enabled = true;
+                UpdateSlotText(i);
                 return true;
             }
         }
         return false;
+    }
+
+    /// <summary>
+    /// Atualiza a contagem dos items de um slot do inventário.
+    /// </summary>
+    /// <param name="i">Índice do slot</param>
+    public void UpdateSlotText(int i)
+    {
+        var slotScript = slots[i].gameObject.GetComponent<Slot>();
+        var quantidadeTexto = slotScript.qtdTexto;
+        quantidadeTexto.enabled = true;
+        quantidadeTexto.text = items[i].quantidade.ToString();
     }
 }
