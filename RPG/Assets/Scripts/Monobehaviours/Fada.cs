80d3e1dcb7a822609083002cf1e3b2feffd2bdd2
Jonathan Takeshi Ywahashi <jonathan.ywahashi@gmail.com>
2022-05-15 21:18:54 -0300

    fix: fixes NPC dialogs

--- RPG/Assets/Scripts/Monobehaviours/Fada.cs
+++ RPG/Assets/Scripts/Monobehaviours/Fada.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
+using System.Linq;
 
 /// <summary>
 /// Gerencia o NPC fada.
@@ -9,68 +11,37 @@ public class Fada : MonoBehaviour
     // Balão de fala.
     public GameObject balaoFala;
 
-    // Texto a ser exibido.
-    public GameObject texto;
+    // Objeto com o texto de exibição.
+    public GameObject conteudoObject;
 
-    // Biblioteca para lidar melhor com texto.
-    private TextMeshPro textMeshPro;
+    // Objeto com o texto de "proximo".
+    public GameObject proximoObject;
+
+    // Classe que possui as sentenças para diálogo.
+    public Dialogue dialogue;
+
+    // Texto de exibição.
+    TextMeshPro conteudo;
 
     // Collider da fada.
-    private CircleCollider2D circleCollider2D;
+    CircleCollider2D circleCollider2D;
 
-    // Dica sendo exibida atualmente.
-    private int numeroDica;
+    // Armazena as sentenças do dialógo.
+    Queue<string> sentences;
 
-    private int contador;
+    // Indica se o balão de fala está sendo mostrado.
+    bool conversando = false;
 
     /// <summary>
     /// Inicialização do script.
     /// </summary>
     private void Awake()
     {
-        circleCollider2D = GameObject.Find("Fada").GetComponent<CircleCollider2D>();
-        textMeshPro = texto.GetComponent<TextMeshPro>();
+        circleCollider2D = GetComponent<CircleCollider2D>();
+        conteudo = conteudoObject.GetComponent<TextMeshPro>();
         balaoFala.SetActive(false);
-        texto.SetActive(false);
-        numeroDica = 0;
-        contador = 26;
-
-    }
-
-    /// <summary>
-    /// Configura o texto a ser exibido.
-    /// </summary>
-    /// <param name="text"></param>
-    private void SetupText(string text)
-    {
-        textMeshPro.SetText(text);
-        textMeshPro.ForceMeshUpdate();
-    }
-
-    /// <summary>
-    /// Controlador de dica da fada.
-    /// </summary>
-    private void EstagioConversa() // NAO Esta Funcionando
-    {
-        if (numeroDica == 1)
-        {
-            SetupText("Para completar o seu pote de ouro\nvoc� precisar� enfrentar o BOSS!\n(Aperte E para pr�xima Dica)");
-        }
-        else if (numeroDica == 2)
-        {
-            SetupText("Cuidado! Voc� precisa estar\nbem forte para enfrent�-lo!\n(Aperte E para pr�xima Dica)");
-        }
-        else
-        {
-            SetupText("Ache a po��o m�gica\n antes que ele te ache.\nVoc� ganhar� super poderes!");
-        }
-
-        numeroDica++;
-
-        if (numeroDica > 3)
-        {
-            numeroDica = 1;
-        }
+        proximoObject.SetActive(true);
+        sentences = new Queue<string>();
     }
 
     /// <summary>
@@ -79,11 +50,9 @@ public class Fada : MonoBehaviour
     /// <param name="collision">Objeto colidido.</param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            balaoFala.SetActive(true);
-            texto.SetActive(true);
-            SetupText("Para completar o seu pote de ouro\nvoc� precisar� enfrentar o BOSS!\nAche a po��o m�gica para enfrent�-lo!");
+            IniciarConversa(dialogue, gameObject);
         }
     }
 
@@ -93,10 +62,70 @@ public class Fada : MonoBehaviour
     /// <param name="collision">Objeto colidido.</param>
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
-            balaoFala.SetActive(false);
-            texto.SetActive(false);
+            FinalizarConversa();
+        }
+    }
+
+    /**
+     * Configura o texto a ser exibido.
+     * text é a sentença a ser exibida.
+     */
+    private void SetupSentence(string text)
+    {
+        conteudo.SetText(text);
+        conteudo.ForceMeshUpdate();
+    }
+
+    /**
+     * Inicializa a conversa da fada.
+     */
+    public void IniciarConversa(Dialogue dialogue, GameObject obj)
+    {
+        sentences.Clear();
+        dialogue.sentences.ToList().ForEach(e => sentences.Enqueue(e));
+        SetupSentence(sentences.Dequeue());
+
+        balaoFala.SetActive(true);
+        proximoObject.SetActive(true);
+        conversando = true;
+    }
+
+    /**
+     * Finaliza a conversa da fada.
+     */
+    public void FinalizarConversa()
+    {
+        balaoFala.SetActive(false);
+        conversando = false;
+    }
+
+    /**
+     * Continua a conversa da fada.
+     */
+    public void ContinuarConversa()
+    {
+        if (sentences.Count <= 0)
+        {
+            return;
+        }
+
+        var sentence = sentences.Dequeue();
+        SetupSentence(sentence);
+
+        if (sentences.Count <= 0)
+        {
+            proximoObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && conversando)
+        {
+            ContinuarConversa();
         }
     }
 }
